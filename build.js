4af3f3449f17df7b48312d02ecb39c3d4a4cd378
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-05-23 18:36:51 -0400

    Add donate link

--- build.js
+++ build.js
@@ -18,10 +18,10 @@ function getCurrentPlatform(){
 builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
-        appId: 'helioslauncher',
-        productName: 'Helios Launcher',
+        appId: 'moontwolauncher',
+        productName: 'MOON2 Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
-        copyright: 'Copyright © 2018-2020 Daniel Scalzi',
+        copyright: 'Copyright © 2018-2020 Daniel Scalzi, Portions are © 2015-2020 MOONMOON',
         directories: {
             buildResources: 'build',
             output: 'dist'
