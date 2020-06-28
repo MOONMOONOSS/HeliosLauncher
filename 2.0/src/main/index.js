87bf04bd22b56d98791dd510f707f53e71e48d0b
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-06-28 14:15:05 -0400

    Create menu items on MacOS

--- 2.0/src/main/index.js
+++ 2.0/src/main/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron' // eslint-disable-line
+import { app, BrowserWindow, Menu } from 'electron' // eslint-disable-line
 import path from 'path';
 
 /**
@@ -41,10 +41,83 @@ function createWindow() {
   });
 }
 
+function createMenu() {
+  if (process.platform === 'darwin') {
+    // Extend default included application menu to continue support for quit keyboard shortcut
+    const applicationSubMenu = {
+      label: 'Application',
+      submenu: [
+        {
+          label: 'About Application',
+          selector: 'orderFrontStandardAboutPanel:',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          },
+        },
+      ],
+    };
+
+    // New edit menu adds support for text-editing keyboard shortcuts
+    const editSubMenu = {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          selector: 'undo:',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          selector: 'redo:',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          selector: 'cut:',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          selector: 'copy:',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          selector: 'paste:',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          selector: 'selectAll:',
+        },
+      ],
+    };
+
+    // Bundle submenus into a single template and build a menu object with it
+    const menuTemplate = [applicationSubMenu, editSubMenu];
+    const menuObject = Menu.buildFromTemplate(menuTemplate);
+
+    // Assign it to the application
+    Menu.setApplicationMenu(menuObject);
+  }
+}
+
 // https://github.com/electron/electron/issues/18397
 app.allowRendererProcessReuse = true;
 
 app.on('ready', createWindow);
+app.on('ready', createMenu);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
