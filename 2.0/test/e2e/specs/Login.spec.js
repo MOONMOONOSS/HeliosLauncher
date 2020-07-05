a96ee8994514c1f6654745954287b46dd3d3ae62
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-07-05 13:22:08 -0400

    Suppress lint errors to allow building

--- 2.0/test/e2e/specs/Login.spec.js
+++ 2.0/test/e2e/specs/Login.spec.js
@@ -1,21 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-expressions */
 import utils from '../utils';
 
 describe('Login', function () {
   beforeEach(utils.beforeEach);
   afterEach(utils.afterEach);
 
-  it.skip('navigates to whitelist view after successful login', async function () {    
-    //From launch, navigate to login page
+  it.skip('navigates to whitelist view after successful login', async function () {
+    // From launch, navigate to login page
     await this.app.client.element('#welcomeButton').click();
     const loginPageElement = await this.app.client.element('#loginContainer');
     expect(loginPageElement.value).to.exist;
 
-    const email = '#mcUsername'
-    const password = '#mcPassword'
-    const loginButton = await this.app.client.element('#loginButton')
+    const email = '#mcUsername';
+    const password = '#mcPassword';
+    const loginButton = await this.app.client.element('#loginButton');
 
-    await this.app.client.setValue('#mcUsername', 'myEmail@email.com')
-    await this.app.client.setValue('#mcPassword', 'fakePAssword')
+    await this.app.client.setValue('#mcUsername', 'myEmail@email.com');
+    await this.app.client.setValue('#mcPassword', 'fakePAssword');
     await loginButton.click();
     const whitelistElement = await this.app.client.element('.whitelistButton');
     expect(whitelistElement.value).to.exist;
