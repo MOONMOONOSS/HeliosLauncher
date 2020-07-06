6389befd570244771206089dd18aa16010fccaad
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-07-06 18:21:33 -0400

    Enable loader div

--- 2.0/src/renderer/App.vue
+++ 2.0/src/renderer/App.vue
@@ -11,6 +11,21 @@ import FrameBar from './components/electron/FrameBar.vue';
 export default {
   name: 'launcher',
   components: { FrameBar },
+  mounted() {
+    this.$nextTick(async () => {
+      const loader = document.getElementById('loadingContainer');
+      loader.style.opacity = 0;
+
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          loader.style.visibility = 'hidden';
+          resolve();
+        }, 500);
+      });
+
+      this.$el.style.opacity = 1;
+    });
+  },
 };
 </script>
 
@@ -24,7 +39,9 @@ export default {
   src url(../../static/fonts/Avenir-Medium.ttf)
 
 body
+  height 100vh
   margin 0
+  overflow hidden
   transition background-image 1s ease
   user-select none
 
@@ -40,9 +57,11 @@ main
   background-size cover
   /* display none */
   height -webkit-fill-available
-  position absolute
+  opacity 0
+  transition all .85s ease
+  transition-delay .5s
   width 100%
-  z-index 10
+  z-index 0
 
 #app[overlay]
   filter blur(3px) contrast(.9) brightness(1.0)
