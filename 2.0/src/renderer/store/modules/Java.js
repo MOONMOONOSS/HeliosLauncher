514fdd713599a4b66cc8d219c5d414334b0f5d33
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-07-30 16:27:35 -0400

    Move Java state to own VueX module

---
+++ 2.0/src/renderer/store/modules/Java.js
@@ -0,0 +1,45 @@
+import { ipcRenderer } from 'electron'; // eslint-disable-line
+
+const storage = window.localStorage;
+
+const state = {
+  javaExe: storage.getItem('java-executable'),
+};
+
+const mutations = {
+  setJavaExe(state, val) {
+    state.javaExe = val;
+
+    if (!val) {
+      storage.removeItem('java-executable');
+      return;
+    }
+
+    storage.setItem('java-executable', val);
+  },
+};
+
+const getters = {
+  selectedJavaExe: (state) => state.javaExe,
+  javaDetails: (_state, getters, _rootState, rootGetters) => async () => {
+    const details = await ipcRenderer.invoke('java-details', {
+      exe: getters.selectedJavaExe,
+      mcVersion: rootGetters['Distribution/selectedServer'].minecraftVersion,
+    });
+
+    console.dir(details);
+
+    return details;
+  },
+};
+
+const actions = {
+};
+
+export default {
+  state,
+  mutations,
+  getters,
+  actions,
+  namespaced: true,
+};
--- 2.0/src/renderer/store/modules/Landing.js
+++ 2.0/src/renderer/store/modules/Landing.js
@@ -1,12 +1,9 @@
 import { ipcRenderer } from 'electron'; // eslint-disable-line
 
-const storage = window.localStorage;
-
 const state = {
   settingsVisibility: false,
   skinVisibility: false,
   serverVisibility: false,
-  javaExe: storage.getItem('java-executable'),
 };
 
 const mutations = {
@@ -19,16 +16,6 @@ const mutations = {
   serverVisibility(state, val) {
     state.serverVisibility = val;
   },
-  setJavaExe(state, val) {
-    state.javaExe = val;
-
-    if (!val) {
-      storage.removeItem('java-executable');
-      return;
-    }
-
-    storage.setItem('java-executable', val);
-  },
 };
 
 const getters = {
@@ -40,17 +27,6 @@ const getters = {
     const data = await ipcRenderer.invoke('minecraft-server', JSON.stringify(payload));
 
     return JSON.parse(data);
-  },
-  selectedJavaExe: (state) => state.javaExe,
-  javaDetails: (_state, getters, _rootState, rootGetters) => async () => {
-    const details = await ipcRenderer.invoke('java-details', {
-      exe: getters.selectedJavaExe,
-      mcVersion: rootGetters['Distribution/selectedServer'].minecraftVersion,
-    });
-
-    console.dir(details);
-
-    return details;
   },
 };
 
