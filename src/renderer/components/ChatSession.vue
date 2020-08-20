b8768cbe16230100a047341143c407a1fb0db1a5
Joseph Zurowski <12567900+Dunklheit@users.noreply.github.com>
2020-08-20 16:57:48 -0400

    Add chat GUI basic styling

--- src/renderer/components/ChatSession.vue
+++ src/renderer/components/ChatSession.vue
@@ -11,4 +11,10 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+main
+  align-self flex-end
+  background unset
+  background-color rgba(0,0,0,.09)
+  flex-grow 1
+  letter-spacing 1.25px
 </style>
