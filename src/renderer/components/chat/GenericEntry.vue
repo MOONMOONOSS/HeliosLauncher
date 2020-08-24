16b3423e2f627d207b4379ed01b519290eae1dc6
Joseph Zurowski <12567900+Dunklheit@users.noreply.github.com>
2020-08-24 14:03:06 -0400

    Bubble up rendered event to ChatSession

--- src/renderer/components/chat/GenericEntry.vue
+++ src/renderer/components/chat/GenericEntry.vue
@@ -7,6 +7,7 @@
     :prerender="preRender"
     :postrender="postRender"
     class="chat-entry"
+    @rendered="$emit('rendered')"
   >
     {{ contents }}
   </vue-markdown>
@@ -32,6 +33,7 @@ export default {
       }),
     },
   },
+  emits: ['rendered'],
   data: () => ({
     hideKeyframes: [
       { // from
